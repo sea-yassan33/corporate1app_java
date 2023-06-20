111b6468542ac821cf169b5967681e0fd0bec464
Yasuda <a.yasuda_tr51@me.com>
2023-06-20 17:36:40 +0900

    first_commit

---
+++ src/main/java/com/example/corporate1app/Corporate1appApplication.java
@@ -0,0 +1,13 @@
+package com.example.corporate1app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Corporate1appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Corporate1appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/corporate1app/Corporate1appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corporate1app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Corporate1appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
